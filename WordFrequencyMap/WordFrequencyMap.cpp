59a0ee3e00ede00e9fa908b6ca2cee0ccd2120a8
Flammino <Flammino@gmail.com>
2017-08-09 02:38:10 -0400

    Prints map, moves to vector, sorts by frequency, prints vector

--- WordFrequencyMap/WordFrequencyMap.cpp
+++ WordFrequencyMap/WordFrequencyMap.cpp
@@ -20,13 +20,24 @@ void frequencyCount(std::map <std::string,int> &map, std::string word) // Pass b
 	}
 }
 
+// Checks if value of digits in string A is greater then the value of digits in string B
+bool compare(std::string a, std::string b)
+{
+	a.erase(remove_if(a.begin(), a.end(), isalpha), a.end()); // Removes letters
+	b.erase(remove_if(b.begin(), b.end(), isalpha), b.end());
+	int aInt = stoi(a);
+	int bInt = stoi(b);
+	return aInt > bInt;
+}
+
 int main()
 {
 	// Note: file to read can't have a BOM. If you need to remove it, use notepad++
 	std::string docToRead = "WarOfTheWorldsExcerpt.txt"; // Must be in same directory as cpp file
 	std::map <std::string, int> wordMap;
 	std::string word;
-	std::string wordNoPunct;
+	std::string count;
+	std::vector<std::string> vect;
 	std::ifstream file;
 	file.open(docToRead);
 	if (!file.is_open())
@@ -35,13 +46,31 @@ int main()
 	}
 	while (file >> word)
 	{
-		word.erase(std::remove_if(word.begin(), word.end(), ispunct), word.end()); // Removes punctuation
+		word.erase(remove_if(word.begin(), word.end(), ispunct), word.end()); // Removes punctuation
+		word.erase(remove_if(word.begin(), word.end(), isdigit), word.end()); // Removes digits
 		if (word != "") // Prevents issues when just punctuation was grabbed
 		{
 			word[0] = tolower(word[0]); // Ensures first letter of word is lower case
 			frequencyCount(wordMap, word);
-		}	
+		}
 	}
+	int size = wordMap.size();
+	std::cout << "There are " << size << " unique words.\n\n";
+	std::cout << "Alphabetical order:\n\n";
+	for (std::map<std::string, int>::iterator it = wordMap.begin(); it != wordMap.end(); ++it)
+	{
+		word = (it->first);
+		count = std::to_string(it->second);
+		std::cout << word << " appears " << count << " times\n";
+		vect.push_back(count + ' ' + word); // Adds count then word to vector for sorting
+	}
+	sort(vect.begin(), vect.end(), compare);
+	std::cout << "\n\n\nIn order from most common to least common:\n\n";
+	for (int i = 0; i < size; i++)
+	{
+		std::cout << vect.at(i) << std::endl;
+	}
+	
     return 0;
 }
 
