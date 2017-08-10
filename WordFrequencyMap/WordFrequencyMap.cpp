f89e988e8a38c4c494041670082f970ace1468ee
Flammino <Flammino@gmail.com>
2017-08-10 15:36:03 -0400

    Added options based on user input, outputs to text file

--- WordFrequencyMap/WordFrequencyMap.cpp
+++ WordFrequencyMap/WordFrequencyMap.cpp
@@ -1,7 +1,9 @@
 // WordFrequencyMap.cpp
-// Adam Flammino 8/8 SDEV345
-// Uses a map to count the frequency of words in a document, displays frequency, 
-// moves map to an array, sorts by decreasing order of frequency, displays that.
+// Adam Flammino 8/8
+// Uses a map to count the frequency of words in a document, 
+// moves map to an array, sorts by decreasing order of frequency.
+// Once data is gathered, gives user the option to display in console
+// and/or save to a file.
 
 #include "stdafx.h"
 #include <map>
@@ -32,44 +34,103 @@ bool compare(std::string a, std::string b)
 
 int main()
 {
-	// Note: file to read can't have a BOM. If you need to remove it, use notepad++
-	std::string docToRead = "WarOfTheWorldsExcerpt.txt"; // Must be in same directory as cpp file
+	std::string docToRead; // Must be in same directory as cpp file
+	std::string docToWrite;
 	std::map <std::string, int> wordMap;
 	std::string word;
 	std::string count;
 	std::vector<std::string> vect;
-	std::ifstream file;
-	file.open(docToRead);
-	if (!file.is_open())
+	std::ifstream inputFile;
+	std::ofstream outputFile;
+	bool run = true;
+	std::string answer;
+	std::string output;
+	while (run)
 	{
-		std::cout << "Unable to open file. Is " << docToRead << " in the same directory as WordFrequencyMap.cpp?";
-	}
-	while (file >> word)
-	{
-		word.erase(remove_if(word.begin(), word.end(), ispunct), word.end()); // Removes punctuation
-		word.erase(remove_if(word.begin(), word.end(), isdigit), word.end()); // Removes digits
-		if (word != "") // Prevents issues when just punctuation was grabbed
+		output = "\n";
+		std::cout << "\nEnter input document name:\t"; // Note: file to read can't have a BOM. If you need to remove it, use notepad++
+		std::cin >> docToRead;
+		inputFile.open(docToRead);
+		if (!inputFile.is_open())
 		{
-			word[0] = tolower(word[0]); // Ensures first letter of word is lower case
-			frequencyCount(wordMap, word);
+			std::cout << "Unable to open file. Is " << docToRead << " in the same directory as WordFrequencyMap.cpp?\n";
 		}
+		else
+		{
+			while (inputFile >> word)
+			{
+				word.erase(remove_if(word.begin(), word.end(), ispunct), word.end()); // Removes punctuation
+				word.erase(remove_if(word.begin(), word.end(), isdigit), word.end()); // Removes digits
+				if (word != "") // Prevents issues when just punctuation was grabbed
+				{
+					word[0] = tolower(word[0]); // Ensures first letter of word is lower case
+					frequencyCount(wordMap, word);
+				}
+			}
+			inputFile.close();
+			int size = wordMap.size();
+			output += "There are "; 
+			output += std::to_string(size);
+			output += " unique words.\n\n";
+			output += "Alphabetical order:\n\n";
+			for (std::map<std::string, int>::iterator it = wordMap.begin(); it != wordMap.end(); ++it)
+			{
+				word = (it->first);
+				count = std::to_string(it->second);
+				output += word;
+				output += " appears ";
+				output += count;
+				output += " times\n";
+				vect.push_back(count + ' ' + word); // Adds count then word to vector for sorting
+			}
+			sort(vect.begin(), vect.end(), compare);
+			output += "\n\n\nIn order from most common to least common:\n\n";
+			for (int i = 0; i < size; i++)
+			{
+				output += vect.at(i);
+				output += "\n";
+			}
+			answer = "0";
+			while (answer != "yes" && answer != "no")
+			{
+				std::cout << "\nDisplay results in console? Please enter yes or no.\n";
+				std::cin >> answer;
+				if (answer == "yes")
+				{
+					std::cout << std::endl << output;
+				}
+			}
+			answer = "0";
+			while (answer != "yes" && answer != "no")
+			{
+				std::cout << "\nSave results to txt file? Please enter yes or no.\n";
+				std::cin >> answer;
+				if (answer == "yes")
+				{
+					docToWrite = "output.";
+					docToWrite += docToRead;
+					std::cout << "Writing results to " << docToWrite << std::endl;
+					outputFile.open(docToWrite);
+					outputFile << output;
+					outputFile.close();
+				}
+			}
+		}
+		answer = "0";
+		while (answer != "yes" && answer != "no")
+		{
+			std::cout << "\nRun again? Please enter yes or no.\n";
+			std::cin >> answer;
+			if (answer == "no")
+			{
+				run = false;
+			}
+		}
+		wordMap.clear();
+		vect.clear();
 	}
-	int size = wordMap.size();
-	std::cout << "There are " << size << " unique words.\n\n";
-	std::cout << "Alphabetical order:\n\n";
-	for (std::map<std::string, int>::iterator it = wordMap.begin(); it != wordMap.end(); ++it)
-	{
-		word = (it->first);
-		count = std::to_string(it->second);
-		std::cout << word << " appears " << count << " times\n";
-		vect.push_back(count + ' ' + word); // Adds count then word to vector for sorting
-	}
-	sort(vect.begin(), vect.end(), compare);
-	std::cout << "\n\n\nIn order from most common to least common:\n\n";
-	for (int i = 0; i < size; i++)
-	{
-		std::cout << vect.at(i) << std::endl;
-	}
+	
+	
 	
     return 0;
 }
